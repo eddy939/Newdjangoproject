75ac7c548c5ba2d065b3bad67702c0285794a064
eddy939 <eddyonyango@gmail.com>
2015-09-11 00:07:42 +0300

    end of chapter 13

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -236,8 +236,8 @@ def clean(self):
 
 def index(request):
 
-    category_list = Category.objects.order_by('-likes')[:5]
-    page_list = Page.objects.order_by('-views')[:5]
+    category_list = Category.objects.order_by('-likes')[:7]
+    page_list = Page.objects.order_by('-views')[:7]
 
     context_dict = {'categories': category_list, 'pages': page_list}
 
